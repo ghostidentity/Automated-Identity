094f081bc2fd50520d5aeee570a9f293b7bb9dcb
Mark <mtagab@outlook.com>
2023-07-07 21:30:24 +0800

    Update ConfigData.cs

--- mac/ConfigData.cs
+++ mac/ConfigData.cs
@@ -1,9 +1,7 @@
-namespace Identity
+using System;
+public class ConfigData
 {
-	public class ConfigData
-	{
-		public string url { get; set; }
-		public string encryptioncode { get; set; }
-		public string signaturecode { get; set; }
-	}
+    public string url { get; set; }
+    public string encryptioncode { get; set; }
+    public string signaturecode { get; set; }
 }
