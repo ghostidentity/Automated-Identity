45bea3338f9216a1033f3f5f82fb3ce5c3c20da9
Mark <mtagab@outlook.com>
2023-07-07 21:29:58 +0800

    Update MacReader.cs

--- mac/MacReader.cs
+++ mac/MacReader.cs
@@ -1,107 +1,103 @@
+using System;
+using System.IO;
+using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Text.Json;
-namespace Identity
+using System.Threading.Tasks;
+
+public class MacReader
 {
+	[DllImport("libX11")]
+	private static extern bool SetForegroundWindow(IntPtr hWnd);
 
-	public class MacReader
+	private ConfigData configData;
+	private readonly System.Net.Http.HttpClient httpClient = new System.Net.Http.HttpClient();
+
+	public void StartQRCodeDetection()
 	{
-		[DllImport("libX11")]
-		private static extern bool SetForegroundWindow(IntPtr hWnd);
+		Console.WriteLine("QR code detection started...");
+		var focusTask = Task.Run(FocusConsoleWindow);
 
-		private ConfigData configData;
-		private readonly HttpClient httpClient = new HttpClient();
-
-		public void StartQRCodeDetection()
+		while (true)
 		{
-			Console.WriteLine("QR code detection started...");
-			var focusTask = Task.Run(FocusConsoleWindow);
-
-			while (true)
-			{
-				var qrCodeValue = Console.ReadLine();
-				HandleQRCodeValue(qrCodeValue);
-			}
-		}
-
-		private void FocusConsoleWindow()
-		{
-			while (true)
-			{
-				// No equivalent action needed for Linux
-				Task.Delay(500).Wait();
-			}
-		}
-
-		private async void HandleQRCodeValue(string qrCodeValue)
-		{
-			await SendHttpRequestAsync(qrCodeValue);
-		}
-
-		private async Task SendHttpRequestAsync(string qrCodeValue)
-		{
-			if (configData == null)
-			{
-				return;
-			}
-
-			try
-			{
-				var requestData = new
-				{
-					encryptioncode = configData.encryptioncode,
-					signaturecode = configData.signaturecode,
-					identificationcode = qrCodeValue
-				};
-
-				var requestJson = JsonSerializer.Serialize(requestData);
-				var content = new StringContent(requestJson, System.Text.Encoding.UTF8, "application/json");
-
-				var response = await httpClient.PostAsync(configData.url, content);
-				response.EnsureSuccessStatusCode();
-
-				var responseJson = await response.Content.ReadAsStringAsync();
-				var responseData = JsonSerializer.Deserialize<ResponseData>(responseJson);
-
-				Console.WriteLine($"Response received: {responseJson}");
-				// Process the response data as needed
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"An error occurred while sending the HTTP request: {ex.Message}");
-				// Handle the exception accordingly
-			}
-		}
-
-		public void ReadConfigFile()
-		{
-			try
-			{
-				string configFile = "config.txt";
-
-				if (File.Exists(configFile))
-				{
-					string configJson = File.ReadAllText(configFile);
-					configData = JsonSerializer.Deserialize<ConfigData>(configJson);
-
-					if (configData.encryptioncode != null && configData.signaturecode != null)
-					{
-						Console.WriteLine("Configuration file is valid");
-					}
-				}
-				else
-				{
-					Console.WriteLine("Config file not found!");
-					// Handle the absence of the config file accordingly
-				}
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine($"An error occurred while reading the config file: {ex.Message}");
-				// Handle the exception accordingly
-			}
+			var qrCodeValue = Console.ReadLine();
+			HandleQRCodeValue(qrCodeValue);
 		}
 	}
 
+	private void FocusConsoleWindow()
+	{
+		while (true)
+		{
+			// No equivalent action needed for Linux
+			Task.Delay(500).Wait();
+		}
+	}
 
+	private async void HandleQRCodeValue(string qrCodeValue)
+	{
+		await SendHttpRequestAsync(qrCodeValue);
+	}
+
+	private async Task SendHttpRequestAsync(string qrCodeValue)
+	{
+		if (configData == null)
+		{
+			return;
+		}
+
+		try
+		{
+			var requestData = new
+			{
+				encryptioncode = configData.encryptioncode,
+				signaturecode = configData.signaturecode,
+				identificationcode = qrCodeValue.Replace("\"", "")
+			};
+
+			var requestJson = JsonSerializer.Serialize(requestData);
+			var content = new StringContent(requestJson, System.Text.Encoding.UTF8, "application/json");
+
+			var response = await httpClient.PostAsync(configData.url, content);
+			response.EnsureSuccessStatusCode();
+
+			var responseJson = await response.Content.ReadAsStringAsync();
+			Console.WriteLine($"Response received: {responseJson}");
+			await Task.Delay(2000);
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"An error occurred while sending the HTTP request: {ex.Message}");
+			// Handle the exception accordingly
+		}
+	}
+
+	public void ReadConfigFile()
+	{
+		try
+		{
+			string configFile = "config.txt";
+
+			if (File.Exists(configFile))
+			{
+				string configJson = File.ReadAllText(configFile);
+				configData = JsonSerializer.Deserialize<ConfigData>(configJson);
+
+				if (configData.encryptioncode != null && configData.signaturecode != null)
+				{
+					Console.WriteLine("Configuration file is valid");
+				}
+			}
+			else
+			{
+				Console.WriteLine("Config file not found!");
+				// Handle the absence of the config file accordingly
+			}
+		}
+		catch (Exception ex)
+		{
+			Console.WriteLine($"An error occurred while reading the config file: {ex.Message}");
+			// Handle the exception accordingly
+		}
+	}
 }
-
